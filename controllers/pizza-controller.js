07514888e8bbd459f5e184d02ce6bb50041fd9e4
Angela Man <angela.k.man@gmail.com>
2021-07-03 19:31:05 -0500

    add and remove comments

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -3,7 +3,14 @@ const { Pizza } = require('../models');
 const pizzaController = {
     // get all pizzas
     getAllPizza(req, res) {
+        console.log(Pizza);
         Pizza.find({})
+            .populate({
+                path: 'comments',
+                select: '-__v'
+            })
+            .select('-__v')
+            .sort({ _id: -1 }) // sort in DESC order
             .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
@@ -14,6 +21,11 @@ const pizzaController = {
     // get one pizza by id
     getPizzaById({ params },res) {
         Pizza.findOne({_id: params.id})
+            .populate({
+                path: 'comments',
+                select: '- __v'
+            })
+            .select('-__v')
             .then(dbPizzaData => {
                 if (!dbPizzaData) {
                     res.status(404).json({ message: 'No pizza found with this id' });
--- server.js
+++ server.js
@@ -10,13 +10,22 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
+
+console.log("first", mongoose.connection.readyState);
+
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/pizza-hunt', {
     useFindAndModify: false,
     useNewUrlParser: true,
     useUnifiedTopology: true
-});
+})
+.then(()=> console.log('database connected', mongoose.connection.readyState))
+.catch(err => console.log("error", err));
+
+
+console.log("after", mongoose.connection.readyState);
 
 // use this to log mongo queries being executed
 mongoose.set('debug', true);
+
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
